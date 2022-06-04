a6eb652d4ed53447c18b3bc72b7a2c9535729b49
Sumuyzzz <Sumuyzzz@gmail.com>
2022-06-04 16:32:28 +0800

    自适应屏幕16/9

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import DataVVue3 from '@kjgl77/datav-vue3'
+import './normalize.scss'
 
 
 
@@ -8,6 +8,5 @@ import DataVVue3 from '@kjgl77/datav-vue3'
 
 
 const app = createApp(App)
-app.use(DataVVue3)
 app.mount('#app')
   
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postcssPxToViewport from "postcss-px-to-viewport"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [postcssPxToViewport({
+        viewportWidth: 1920,
+        exclude: [/node_modules/, /\/src\/assets\//, /index.html/],
+      })]
+    }
+  }
 })
