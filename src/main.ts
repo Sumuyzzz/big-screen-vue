940505dd8af49caabf3fb60ce9d58761433f903a
Sumuyzzz <Sumuyzzz@gmail.com>
2022-06-04 13:07:21 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import DataVVue3 from '@kjgl77/datav-vue3'
+
+
+
+
+
+
+const app = createApp(App)
+app.use(DataVVue3)
+app.mount('#app')
+  
