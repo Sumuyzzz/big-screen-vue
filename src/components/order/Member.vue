dbb6db164858319a4baccc7f4f2532b2dfa1cd74
Sumuyzzz <Sumuyzzz@gmail.com>
2022-06-09 15:22:57 +0800

    完成所有组件

---
+++ src/components/order/Member.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :class="color" class="box1">
+    <v-icon :name="names" color="#fff" class="icon" />
+    <div class="message">
+      <span>{{ text }}</span>
+      <h2>{{ value }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  text: string,
+  value: string,
+  color: string,
+  names: string,
+}>()
+
+</script>
+
+<style lang="scss" scoped>
+.box1 {
+  width: 294px;
+  height: 124px;
+  margin: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .message {
+    color: #fff;
+    font-size: 0.2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+  }
+  .icon {
+    height: 50px;
+    width: 50px;
+  }
+}
+
+.red {
+  background: #d9523f;
+}
+
+.green {
+  background: #13d0b2;
+}
+
+.yellow {
+  background: #f6a645;
+}
+</style>
---
+++ src/components/order/Order.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="members">
+    <Member text="本月订单数" value="18000" names="hi-solid-clipboard-list" color="red" />
+    <Member text="本月新增会员" value="1000" names="fa-crown" color="green" />
+    <Member text="一次消费会员" value="600" names="ri-money-cny-circle-fill" color="yellow" />
+  </div>
+</template>
+<script lang="ts" setup >
+
+</script>
+
+
+<style scoped lang="scss">
+.members {
+  width: 330px;
+
+
+}
+</style>
